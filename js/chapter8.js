7c1602d327a20872e6b204991c3ef9276b6899c8
Mauricio <mauriciocarbajali@gmail.com>
2017-03-07 02:52:11 -0300

    capítulo 8 completo

---
+++ js/chapter8.js
@@ -0,0 +1,51 @@
+output('***** CHAPTER 8 - METHODS | Examples from the book "JS - The Good Parts" *****');
+
+output("");
+output("--- Guide:");
+
+output("arr1.concat(arr2,elem,...)");
+output("arr1.join('#')");
+output("last = arr1.pop()");
+output("arr1.push(elem)");
+output("arr1.reverse()");
+output("first = arr1.shift();");
+output("arr1.slice(start,end)");
+output("arr1.sort(comp_func)");
+output("arr1.slice(start,delcount,itemtoreplace)");
+output("arr1.unshift(item,...)");	//push en el ppio
+
+output("func1.apply(thisArg,argsArray)");
+
+output("num.toExponential(fracDigs)");
+output("num.toFixed(fracDigs)");
+output("num.toPrecision(precision)");
+output("num.toString(base)");
+
+output("obj.hasOwnProperty(name)");
+
+output("regexp.exec(string)");	//powerful, slow, return every match
+output("regexp.test(string)");	//fastest, returns bool
+
+output("str.charAt(pos)");		// C
+output("str.charCodeAt(pos)");	// 67
+output("str.concat(str2)");
+output("str.indexOf(searchstr, position)");	//pos || -1
+output("str.lastIndexOf(searchString, position)");
+output("str.localeCompare(that)");
+output("str.match(regexp)");
+output("str.replace(searchValue, replaceValue)");
+output("str.search(regexp)");
+output("str.slice(start, end)");
+output("str.split(separator, limit)");
+output("str.substring(start, end)");
+output("str.toLocaleLowerCase( )");
+output("str.toLocaleUpperCase( )");
+output("str.toLowerCase( )");
+output("str.toUpperCase( )");
+output("str.fromCharCode(char...)");
+
+
+
+
+
+
